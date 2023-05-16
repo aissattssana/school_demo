bc08a922e6a9313748d6004234eb461149a19846
asya <anoshina050503@gmail.com>
2023-05-16 13:15:06 +0300

    доработка

--- school_demo/AdminWin.xaml.cs
+++ school_demo/AdminWin.xaml.cs
@@ -19,9 +19,35 @@ namespace school_demo
     /// </summary>
     public partial class AdminWin : Window
     {
+        public User2Context User2Context = new User2Context();
         public AdminWin()
         {
             InitializeComponent();
+        }
+
+        private void create_Click(object sender, RoutedEventArgs e)
+        {
+            Type a = cost1.Text.GetType();
+
+            if (User2Context.Services.Any(x => x.Name != name1.Text))
+            {
+                if (Convert.ToInt32(time1.Text) < 14400)
+                {
+                    Service usl = new Service { Name = name1.Text, Photo = null, Duration = Convert.ToInt32(time1.Text), Cost =Convert.ToDouble(cost1.Text), Discount = Convert.ToInt16(skidka1.Text) };
+                    User2Context.Services.Add(usl);
+                    User2Context.SaveChanges();
+                    MessageBox.Show("Услуга добавлена");
+                       }
+                else { MessageBox.Show("Продолжительность не может превышать 4 часов(14400 сек)"); }
+            }
+            else { MessageBox.Show("Такая услуга уже существует"); }
+        }
+    
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow m = new MainWindow(true);
+            m.ShowDialog();
         }
     }
 }
